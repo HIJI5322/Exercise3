28bdc93f2455ba7ceefb87263bea632694378857
HIJI <h.12.26.k@icloud.com>
2025-06-27 12:43:39 +0900

    Won/lost message added

--- dicegame.java
+++ dicegame.java
@@ -14,5 +14,12 @@ public class dicegame {
         System.out.println("Die 1: " + val1);
         System.out.println("Die 2: " + val2);
         System.out.println("Total value: " + total);
+
+    if(total > 7){
+        System.out.println("You won!");
+    } else {
+        System.out.println("You lost!");
+    }
+
     }
 }
