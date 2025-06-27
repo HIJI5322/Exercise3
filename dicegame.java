81e5d3b86856a0c931525772b204a8f34b3b871d
HIJI <h.12.26.k@icloud.com>
2025-06-27 12:27:50 +0900

    initial revision

---
+++ dicegame.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class dicegame {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int val1 = 0;
+        int val2 = 0;
+        int total = 0;
+
+        val1 = rand.nextInt(6) + 1;
+        val2 = rand.nextInt(6) + 1;
+        total = val1 + val2;
+
+        System.out.println("Die 1: " + val1);
+        System.out.println("Die 2: " + val2);
+        System.out.println("Total value: " + total);
+    }
+}
